be7f302f3a38e928145cd86e340d264faeccce39
Ove Ljosland <oveljosland@icloud.com>
2025-04-22 15:25:01 +0200

    update

--- d11/d11.ino
+++ d11/d11.ino
@@ -5,25 +5,50 @@
   static const BaseType_t app_cpu = 1;
 #endif
 
-const int btnPin = 12;
-const int ledPin = 13;
+#define DISP_W 128 /* display width */
+#define DISP_H 64 /* display height */
+#define RST -1 /* reset */
 
-/* 
-* BinÃ¦r semafor for Ã¥ sÃ¸rge for at id-nummeret blir lageret i interactor-tasken. 
-* Dette brukes for Ã¥ identifisere hvilken knapp som har blitt trykket inn.
-*/
-static SemaphoreHandle_t binSem;
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-/* Identifikator for interactor-oppgaven */
-static TaskHandle_t interactorTask;
+#define N_PINS 4
 
-/*
-* Avbruddsrutine for knappetrykk. Vekker den pÃ¥koblete interactorTask-trÃ¥den. 
-*/
+#define L1 25
+#define L2 33
+#define L3 32
+#define L4 12
+
+#define B1 13
+#define B2 14
+#define B2 27
+#define B2 25
+
+
+/* check if leds.state[i] == btns.state[i] */
+Adafruit_SSD1306 display(DISP_W, DISP_H, &Wire, RST);
+
+struct port {
+  uint8_t pins[N_PINS];
+  uint8_t states[N_PINS];
+};
+
+struct port btns = {{B1, B2, B3, B4}, {0}};
+struct port leds = {{L1, L2, L3, L4}, {0}};
+
+/* ensures ID is saved in the interactor task, identifies which button is pressed */
+static SemaphoreHandle_t bin_sem;
+
+/* identifier for interactor task */
+static TaskHandle_t interactor_task;
+
+/* ISR for button presses. awakens the interactor task thread */
 void IRAM_ATTR ISR_BTN() {
   BaseType_t task_woken = pdFALSE;
 
-  vTaskNotifyGiveFromISR(interactorTask, &task_woken);
+  vTaskNotifyGiveFromISR(interactor_task, &task_woken);
 
   if (task_woken) {
     portYIELD_FROM_ISR();
@@ -35,47 +60,64 @@ void IRAM_ATTR ISR_BTN() {
 * flere knappe-LED-par ved Ã¥ ha en parameteriserbar ID.  En binÃ¦r semafor brukes for Ã¥ signalisere at IDen er lagret lokalt.
 */
 void interactor(void *parameters) {
-  int interactorId = *(int *)parameters;
-  int ledState = 0;
+  int interactor_id = *(int *)parameters;
 
-  Serial.print("Task ready:");
-  Serial.println(interactorId);
+  for (uint8_t i = 0; i < N_PINS; i++)
+    leds.states[i] = 0;
+
+  Serial.print("task ready:");
+  Serial.println(interactor_id);
   
-  xSemaphoreGive(binSem);
+  xSemaphoreGive(bin_sem);
   while(1) {
     ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-    digitalWrite(ledPin,ledState);
-    ledState = !ledState;
+    for (uint8_t i = 0; i < N_PINS; i++) {
+      digitalWrite(leds.pins[i],leds.states[i]);
+      leds.states[i] = !leds.states[i];
+    }
   }
 }
 
-void setup() {
-    Serial.begin(115200);
-    Serial.println("---FreeRTOS btn and led demo---");
+void setup()
+{
+  Serial.begin(115200);
 
-    char task_name[14];
-    uint8_t interactorId = 0;
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    Serial.println("display: allocation failed");
 
-    binSem = xSemaphoreCreateBinary();
+  display.display();
+  delay(100);
+  display.clearDisplay();
 
-    pinMode(btnPin, INPUT_PULLUP);
-    pinMode(ledPin, OUTPUT);
+  Serial.println("--- reaction trainer ---");
 
-    attachInterrupt(btnPin, ISR_BTN, FALLING);
+  char task_name[14];
+  uint8_t interactor_id = 0;
 
-    sprintf(task_name, "Interactor %i", interactorId);
-    xTaskCreatePinnedToCore(interactor,
-                            task_name,
-                            1024,
-                            (void *)&interactorId,
-                            1,
-                            &interactorTask,
-                            app_cpu);
-    xSemaphoreTake(binSem, portMAX_DELAY);
-  
+  bin_sem = xSemaphoreCreateBinary();
+
+  for (uint8_t i = 0; i < N_PINS; i++) {
+    pinMode(btns.pins[i], INPUT_PULLUP);
+    pinMode(leds.pins[i], OUTPUT);
+  }
+
+  for (uint8_t i = 0; i < N_PINS; i++)
+    attachInterrupt(btns.pins[i], ISR_BTN, FALLING); /* attach interrupts to buttons*/
+
+  sprintf(task_name, "interactor %i", interactor_id);
+  xTaskCreatePinnedToCore(
+    interactor,
+    task_name,
+    1024,
+    (void *)&interactor_id,
+    1,
+    &interactor_task,
+    app_cpu
+    );
+  xSemaphoreTake(bin_sem, portMAX_DELAY);
 }
 
-void loop() {
-
-
+void loop()
+{
+  /* empty */
 }
